583156f2ef67652fea3c7b7e2825c9ddd8f0083d
Your Name <lmiguelapinto@gmail.com>
2021-11-10 11:40:48 +0000

    fixed scripts

--- pipeline/data_splitting.py
+++ pipeline/data_splitting.py
@@ -8,19 +8,13 @@ def main(argv):
 	opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
 
 	dev = pd.read_csv(args[0],delimiter=";")
-
-	print(len(dev))
-	ok = []
-	not_ok = []
-	for t in dev.index:	
-		if(dev["date"][t] > 941000):
-			ok.append(dev['loan_id'][t])
-		else:
-			not_ok.append(dev['loan_id'][t])
 	
-	print(len(not_ok)/(len(ok)+len(not_ok))*100)
-	print(len(ok))
-	print(len(not_ok))
+	train = dev.copy()
+	test = dev.copy()
+
+	test = test.loc[test['date'] < 941000]
+	
+	train = train.loc[(train['date'] >= 941000)]
 	
 if __name__ == "__main__":
    main(sys.argv[1:])
