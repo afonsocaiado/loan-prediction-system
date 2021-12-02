f41a3e67039953f800f5dcc5acb13bc69495b865
elilambr <48095800+vinlir@users.noreply.github.com>
2021-12-02 14:39:04 +0100

    Switched back to right probabilities and simplified inputs

--- pipeline/submission_file.py
+++ pipeline/submission_file.py
@@ -1,12 +1,7 @@
 import collections
 
 def create(competition,classifier):
-	competition_inputs = competition[['loan_date', 'amount', 'duration', 'payments',
-       'frequency', 'account_date', 'region', 'inhabitants',
-       'inhabitants < 499', 'inhabitants 500-1999', 'inhabitants 2000-9999',
-       'inhabitants >10000', 'no. of cities ', 'ratio of urban inhabitants ',
-       'average salary ', 'unemploymant 96',
-       'enterpreneurs', 'crimes 96']].values
+	competition_inputs = competition.drop(['status','loan_id'], axis = 1)
 
     #predict_proba() returns 2Darray of probabilities of classes. We have classes -1: unvalid and 1:valid. We want the class with the unvalid loans
     #using classifier.classes_ we can see the order off classes that predict_proba() returns. We can see that the -1 class is the first element
@@ -14,7 +9,7 @@ def create(competition,classifier):
 
 	d = {}
 	for v in competition.index:
-		d[competition["loan_id"][v]] = round(competition_prob[v][1],1)
+		d[competition["loan_id"][v]] = round(competition_prob[v][0],1)
 	
 	d = collections.OrderedDict(sorted(d.items()))
 
